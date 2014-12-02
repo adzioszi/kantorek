b373fdfed5b737ec26d53e2084aa590cf4934a62
adzioszi <pielgrzym1@onet.eu>
2014-12-02 21:44:41 +0100

    zad1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_gwiazdek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string s = "*";
+                for (int h = 1; h <= 19; h++)
+                {
+                    Console.Write(s);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
