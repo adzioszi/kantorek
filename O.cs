6e95b5aa0d1f5a161384584bde80ffd4318639ec
adzioszi <pielgrzym1@onet.eu>
2014-12-02 21:50:09 +0100

    zad2

---
+++ O.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stożek_O
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("podaj wysokość");
+
+            int m = int.Parse(Console.ReadLine());
+            if (m < 0)
+            {
+                Console.WriteLine("ekhem...");
+            }
+            for (int s = 1; s <= m ; s++)
+            {
+                for (int d = 0; d < s ; d++)
+                {
+
+                    Console.Write("O");
+                }
+                Console.WriteLine();
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
